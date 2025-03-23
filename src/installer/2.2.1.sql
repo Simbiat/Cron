8c372c040dfa3bfdb5fd6fdcb31721e718e8bd21
Dmitrii Kustov <simbiat@outlook.com>
2025-03-23 15:41:36 +0200

    Changed description to VARCHAR

---
+++ src/installer/2.2.1.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `cron__settings` CHANGE `description` `description` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_nopad_ci NULL DEFAULT NULL COMMENT 'Description of the setting';
+UPDATE `cron__settings` SET `value` = '2.2.1' WHERE `cron__settings`.`setting` = 'version';
